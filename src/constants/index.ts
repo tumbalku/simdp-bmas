0790d9efbd0e6ad792c1ed7e783b448e4b7db002
tumbalku <tumbalku00000@gmail.com>
2026-07-13 14:15:36 +0800

    refactor(ui): centralize role badge styles

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,14 @@
 export { APP } from "./app";
 export { DATE_FORMATS, DATE_LOCALE } from "./date";
 export { PAGINATION } from "./pagination";
-export { ROLE_GROUPS, ROLE_LABELS, USER_ROLES, type UserRole } from "./roles";
+export {
+  DEFAULT_ROLE_BADGE_STYLE,
+  ROLE_BADGE_STYLES,
+  ROLE_GROUPS,
+  ROLE_LABELS,
+  USER_ROLES,
+  getRoleBadgeStyle,
+  type UserRole,
+} from "./roles";
 export { ROUTES, routeTo, type RouteKey } from "./routes";
 export { UI } from "./ui";
