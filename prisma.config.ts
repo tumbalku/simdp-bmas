29cf3643566ec815e0181b38bc6d7bcd183ab992
tumbalku <tumbalku00000@gmail.com>
2026-09-20 10:59:05 +0800

    feat(database): initialize v2 schema and migrations

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
-    path: "prisma/migrations",
+    path: "prisma/migrations-v2",
     seed: "npx ts-node --compiler-options {\"module\":\"CommonJS\"} prisma/seed.ts",
   },
   datasource: {
